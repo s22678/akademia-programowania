39f4f40a77cfaceec39f72a57a262dd3aa35e9ce
Marcin O <s22678@pjwstk.edu.pl>
2023-03-29 21:17:37 +0200

    Fetching and saving json, no goroutines, no context

--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"io"
+	"log"
+	"os"
 	"reddit/fetcher"
 )
+
+var (
+	file *os.File
+	err  error
+)
+
+func init() {
+	LOG_FILE := "./app.log"
+	file, err = os.OpenFile(LOG_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+	log.Println("Initialzing reddit fetcher app")
+}
 
 func main() {
 	var f fetcher.RedditFetcher // do not change
 	var w io.Writer             // do not change
 
+	f = &fetcher.Response{}
+	w, err = os.Create("my_output")
+	if err != nil {
+		log.Fatalln("Failed to create my_output file")
+	}
+
+	defer file.Close()
+
 	f.Fetch()
+	f.Save(w)
+
+	w = os.Stdout
 	f.Save(w)
 }
